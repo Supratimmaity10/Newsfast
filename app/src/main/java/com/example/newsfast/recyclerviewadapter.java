105ce55772cee169e13a581f02d849d329229f58
Supratim maity <supratim.maity.alferdo@gmail.com>
2023-01-13 12:08:30 +0530

    change1

--- app/src/main/java/com/example/newsfast/recyclerviewadapter.java
+++ app/src/main/java/com/example/newsfast/recyclerviewadapter.java
@@ -1,22 +1,30 @@
 package com.example.newsfast;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
 public class recyclerviewadapter extends RecyclerView.Adapter<recyclerviewadapter.viewholder> {
     Context context;
-    ArrayList<String> arrayList;
-    public recyclerviewadapter(Context context,ArrayList<String> arrayList){
+    ArrayList<news> newsList;
+    public recyclerviewadapter(Context context,ArrayList<news> newsList){
         this.context=context;
-        this.arrayList=arrayList;
+        this.newsList=newsList;
     }
     @NonNull
     @Override
@@ -28,19 +36,42 @@ public class recyclerviewadapter extends RecyclerView.Adapter<recyclerviewadapte
 
     @Override
     public void onBindViewHolder(@NonNull viewholder holder, int position) {
-        holder.textView.setText(arrayList.get(position));
+      try{
+       holder.title.setText(newsList.get(position).getTitle());
+       holder.description.setText(newsList.get(position).getDescription());
+          Glide.with(context).load(newsList.get(position).urlimage).into(holder.urlimage);
+
+    }catch (Exception e){e.printStackTrace();}
     }
 
     @Override
     public int getItemCount() {
-        return arrayList.size();
+        return newsList.size();
     }
 
-    public class viewholder extends RecyclerView.ViewHolder {
-        TextView textView;
+    public class viewholder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView title,description;
+        ImageView urlimage;
+        LinearLayout layout;
         public viewholder(@NonNull View itemView) {
             super(itemView);
-            textView=itemView.findViewById(R.id.textView);
+            title=itemView.findViewById(R.id.title);
+            description=itemView.findViewById(R.id.description);
+            urlimage=itemView.findViewById(R.id.urlimage);
+        layout=itemView.findViewById(R.id.linearLayout);
+        layout.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            int pos=this.getAdapterPosition();
+            String url=newsList.get(pos).url;
+            customonclick(url);
+        }
+    }
+    public void customonclick(String url){
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+       customTabsIntent.launchUrl(context, Uri.parse(url));
     }
 }
